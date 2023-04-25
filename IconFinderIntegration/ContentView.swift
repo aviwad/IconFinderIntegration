6139313d479378b0783be1247e1eb052b24560a7
Avi Wadhwa <aviwad@gmail.com>
2023-04-25 09:51:44 -0700

    Initial Commit

---
+++ IconFinderIntegration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IconFinderIntegration
+//
+//  Created by Avi Wadhwa on 25/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IconFinderIntegration/IconFinderIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconFinderIntegrationApp.swift
+//  IconFinderIntegration
+//
+//  Created by Avi Wadhwa on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct IconFinderIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
